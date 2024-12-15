d6d7e38f84c32ced1696bb9cba825c232d17b440
moshgui <guilherme14_pvh@hotmail.com>
2024-12-15 15:08:45 -0400

    feat: nav and home components created

--- cotador-saude/src/app/app.module.ts
+++ cotador-saude/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './views/home/home.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent, CardComponent, HomeComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, ButtonsModule.forRoot()],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
